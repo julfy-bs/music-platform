9a0d931e87913925bdee463fb6a3e8a957751b30
julfy-bs <julfy.web@gmail.com>
2022-05-11 18:28:06 +0300

    feat: добавляет server

---
+++ server/dist/main.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5050;
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        app.enableCors();
+        await app.listen(PORT, () => {
+            console.log(`ez ${PORT}`);
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+start();
+//# sourceMappingURL=main.js.map
